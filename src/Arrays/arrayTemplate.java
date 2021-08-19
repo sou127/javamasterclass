8aa7486d68d054b12086d55a786a74f2d6350f48
sou127 <srbh453@gmail.com>
2021-08-20 02:45:21 +0900

    array1

--- src/Arrays/arrayTemplate.java
+++ src/Arrays/arrayTemplate.java
@@ -9,23 +9,34 @@ public class arrayTemplate {
         int[] array = takeInput();
         display(array);
         System.out.println("Max: " + maxInArray(array));
+        System.out.println("Look for: ");
+        int lookFor = scn.nextInt();
+        System.out.println("Linear Search at " + linSearch(array,lookFor) + "th element");
+        System.out.println("Binary Search at " + binSearch(array,lookFor) + "th element");
+
+        bubbleSort(array);
+        display(array);
+        selSort(array);
+        display(array);
+
     }
 
     public static int[] takeInput(){
         System.out.println("Size of Array: ");
         int n = scn.nextInt();
         int[] arr = new int[n];
+        System.out.println("Enter Array Elements: ");
         for (int i = 0; i < n; i++) {
-            System.out.print("Enter " + i + "th element:");
             arr[i] = scn.nextInt();
         }
         return arr;
     }
 
     public static void display(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int j : arr) {
+            System.out.print(j + " ");
         }
+        System.out.println();
     }
 
     public static int maxInArray(int[] arr){
@@ -38,4 +49,59 @@ public class arrayTemplate {
         }
         return max;
     }
+
+    public static int linSearch(int[] arr,int item){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==item)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int binSearch(int[] arr, int item){
+        int low = 0;
+        int high = arr.length;
+        int mid = (low+high)/2;
+        while(low<=high){
+            if(arr[mid]<item){
+                low=mid+1;
+            }else if(arr[mid]>item){
+                high=mid-1;
+            }else{
+                return mid;
+            }
+            mid = (low+high)/2;
+        }
+        return -1;
+    }
+
+    public static void bubbleSort(int[] arr){
+        for (int counter = 0; counter < arr.length-1; counter++) {
+            for (int j = 0; j < arr.length - 1 - counter; j++) {
+                if(arr[j]>arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void selSort(int[] arr){
+        for (int counter = 0; counter < arr.length-1; counter++) {
+            int min = counter;
+            for (int i = counter+1; i < arr.length; i++) {
+                if(arr[i]<arr[min])
+                    min=i;
+            }
+            swap(arr,min,counter);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
 }
